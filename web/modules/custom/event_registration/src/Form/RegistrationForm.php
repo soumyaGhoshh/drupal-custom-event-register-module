975db0b532cb136dd9fa8ed0b34ed5749facfc20
soumyaGhoshh <182009583+soumyaGhoshh@users.noreply.github.com>
2026-01-31 23:33:01 +0530

    Add AJAX for event selection, validatoin, DB save and email notifications

--- web/modules/custom/event_registration/src/Form/RegistrationForm.php
+++ web/modules/custom/event_registration/src/Form/RegistrationForm.php
@@ -5,52 +5,167 @@ namespace Drupal\event_registration\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a public registration form.
+ * Provides a public registration form with validation and email confirmation.
  */
 class RegistrationForm extends FormBase {
 
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
   protected $database;
 
-  public function __construct(Connection $database) {
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The mail manager.
+   *
+   * @var \Drupal\Core\Mail\MailManagerInterface
+   */
+  protected $mailManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new RegistrationForm.
+   */
+  public function __construct(Connection $database, ConfigFactoryInterface $config_factory, MailManagerInterface $mail_manager, AccountProxyInterface $current_user) {
     $this->database = $database;
+    $this->configFactory = $config_factory;
+    $this->mailManager = $mail_manager;
+    $this->currentUser = $current_user;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('database')
+      $container->get('database'),
+      $container->get('config.factory'),
+      $container->get('plugin.manager.mail'),
+      $container->get('current_user')
     );
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId() {
     return 'event_registration_public_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $now = time();
+
+    // Fetch categories that have open registration windows.
     $categories = $this->database->select('event_configuration', 'e')
       ->fields('e', ['category'])
+      ->condition('event_registration_start', $now, '<=')
+      ->condition('event_registration_end', $now, '>=')
       ->distinct()
       ->execute()
       ->fetchCol();
 
+    if (empty($categories)) {
+      $form['message'] = [
+        '#markup' => '<div class="messages messages--warning">' . $this->t('No events are currently open for registration.') . '</div>',
+      ];
+      return $form;
+    }
+
+    $form['#tree'] = TRUE;
+
     $form['category'] = [
       '#type' => 'select',
       '#title' => $this->t('Category of the event'),
-      '#options' => array_combine($categories, $categories),
+      '#options' => ['' => $this->t('- Select Category -')] + array_combine($categories, $categories),
+      '#ajax' => [
+        'callback' => '::updateDateOptions',
+        'wrapper' => 'edit-date-wrapper',
+        'event' => 'change',
+      ],
       '#required' => TRUE,
     ];
 
-    $events = $this->database->select('event_configuration', 'e')
-      ->fields('e', ['id', 'event_name'])
-      ->execute()
-      ->fetchAllKeyed(0, 1);
+    $selected_category = $form_state->getValue('category');
+    $date_options = ['' => $this->t('- Select Category First -')];
 
-    $form['event_id'] = [
+    if ($selected_category) {
+      $dates = $this->database->select('event_configuration', 'e')
+        ->fields('e', ['event_date'])
+        ->condition('category', $selected_category)
+        ->condition('event_registration_start', $now, '<=')
+        ->condition('event_registration_end', $now, '>=')
+        ->distinct()
+        ->execute()
+        ->fetchCol();
+      $date_options = ['' => $this->t('- Select Event Date -')] + array_combine($dates, $dates);
+    }
+
+    $form['event_date_wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'edit-date-wrapper'],
+    ];
+
+    $form['event_date_wrapper']['event_date'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Event Date'),
+      '#options' => $date_options,
+      '#disabled' => empty($selected_category),
+      '#ajax' => [
+        'callback' => '::updateNameOptions',
+        'wrapper' => 'edit-name-wrapper',
+        'event' => 'change',
+      ],
+      '#required' => TRUE,
+    ];
+
+    $selected_date = $form_state->getValue(['event_date_wrapper', 'event_date']);
+    $name_options = ['' => $this->t('- Select Date First -')];
+
+    if ($selected_category && $selected_date) {
+      $events = $this->database->select('event_configuration', 'e')
+        ->fields('e', ['id', 'event_name'])
+        ->condition('category', $selected_category)
+        ->condition('event_date', $selected_date)
+        ->condition('event_registration_start', $now, '<=')
+        ->condition('event_registration_end', $now, '>=')
+        ->execute()
+        ->fetchAllKeyed(0, 1);
+      $name_options = ['' => $this->t('- Select Event Name -')] + $events;
+    }
+
+    $form['event_name_wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'edit-name-wrapper'],
+    ];
+
+    $form['event_name_wrapper']['event_id'] = [
       '#type' => 'select',
       '#title' => $this->t('Event Name'),
-      '#options' => $events,
+      '#options' => $name_options,
+      '#disabled' => empty($selected_date),
       '#required' => TRUE,
     ];
 
@@ -92,9 +207,54 @@ class RegistrationForm extends FormBase {
     return $form;
   }
 
+  public function updateDateOptions(array &$form, FormStateInterface $form_state) {
+    return $form['event_date_wrapper'];
+  }
+
+  public function updateNameOptions(array &$form, FormStateInterface $form_state) {
+    return $form['event_name_wrapper'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $email = $form_state->getValue(['details', 'email']);
+    $event_date = $form_state->getValue(['event_date_wrapper', 'event_date']);
+
+    // Prevent duplicate registrations (Email + Event Date).
+    $duplicate = $this->database->select('event_registration', 'r')
+      ->fields('r', ['id'])
+      ->condition('email', $email)
+      ->condition('event_date', $event_date)
+      ->execute()
+      ->fetchField();
+
+    if ($duplicate) {
+      $form_state->setErrorByName('details][email', $this->t('You have already registered for an event on this date.'));
+    }
+
+    // Special characters check.
+    $text_fields = [
+      'full_name' => $this->t('Full Name'),
+      'college_name' => $this->t('College Name'),
+      'department' => $this->t('Department')
+    ];
+    foreach ($text_fields as $key => $label) {
+      $value = $form_state->getValue(['details', $key]);
+      if (preg_match('/[^a-zA-Z0-9\s.\-,]/', $value)) {
+        $form_state->setErrorByName('details][' . $key, $this->t('@label contains invalid characters.', ['@label' => $label]));
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $event_id = $form_state->getValue('event_id');
+    $event_id = $form_state->getValue(['event_name_wrapper', 'event_id']);
     
+    // Fetch full event details for storage and emails.
     $event_info = $this->database->select('event_configuration', 'e')
       ->fields('e')
       ->condition('id', $event_id)
@@ -117,7 +277,37 @@ class RegistrationForm extends FormBase {
       ->fields($values)
       ->execute();
 
-    $this->messenger()->addStatus($this->t('Registration successful!'));
-  }
+    // Prepare mail params.
+    $params = [
+      'name' => $values['name'],
+      'email' => $values['email'],
+      'event_name' => $values['event_name'],
+      'event_date' => $values['event_date'],
+      'category' => $values['category'],
+      'college' => $values['college'],
+      'department' => $values['department'],
+    ];
+
+    // Send to User.
+    $this->mailManager->mail('event_registration', 'registration_confirmation', $values['email'], 'en', $params, NULL, TRUE);
+
+    // Send to Admin if enabled.
+    $config = $this->configFactory->get('event_registration.settings');
+    if ($config->get('enable_admin_notifications')) {
+      $admin_email = $config->get('admin_email') ?? \Drupal::config('system.site')->get('mail');
+      $this->mailManager->mail('event_registration', 'admin_notification', $admin_email, 'en', $params, NULL, TRUE);
+    }
+
+    $result = $this->mailManager->mail(
+    'event_registration',
+    'registration_confirmation',
+    $values['email'],
+    'en',
+    $params,
+    NULL,
+    TRUE
+  );
+    
+    }
 
 }
